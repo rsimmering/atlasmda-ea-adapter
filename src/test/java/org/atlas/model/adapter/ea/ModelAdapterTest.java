bbe79ec7438fd712fac5b711b3fe2a760966d7ed
Steve Andrews <steve@fountainhead.us.com>
2010-12-06 04:25:50 +0000

    ATLAS-30 Merged metamodel into engine project.

--- src/test/java/org/atlas/model/adapter/ea/ModelAdapterTest.java
+++ src/test/java/org/atlas/model/adapter/ea/ModelAdapterTest.java
@@ -1,12 +1,12 @@
 package org.atlas.model.adapter.ea;
 
 import java.io.File;
-import org.atlas.mda.Context;
-import org.atlas.mda.ModelInput;
-import org.atlas.mda.ModelTransformer;
-import org.atlas.mda.TransformException;
-import org.atlas.mda.adapter.AdapterException;
-import org.atlas.metamodel.Model;
+import org.atlas.engine.Context;
+import org.atlas.engine.ModelInput;
+import org.atlas.engine.ModelTransformer;
+import org.atlas.engine.TransformException;
+import org.atlas.model.adapter.AdapterException;
+import org.atlas.model.metamodel.Model;
 import org.junit.Test;
 
 /**
